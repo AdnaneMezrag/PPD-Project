df2d4c68f80ed2dfd835841ddfb42ed21249365e
Akram Gharbi <akramgharbi13@gmail.com>
2024-04-10 02:07:13 +0100

    FIXED MERGING CONFLICTS

--- Back-End/Server.js
+++ Back-End/Server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json()); //almost forgot this
 const Port = process.env.Port || 4000;
-const loginEndPoints = require("./LoginEndpoints");
-const signipEndPoints = require("./SignupEndpoints");
 app.use(cors());
 
 //==============================LOG and SIGN logic=================================================
-
+const loginEndPoints = require("./LoginEndpoints");
+const signipEndPoints = require("./SignupEndpoints");
 app.use("/api/login", loginEndPoints); // using the other file's endpoints via router to login and recieve full user data
 app.use("/api/signup", signipEndPoints); // using the other file's endpoints via router to singup and add full user data
 
