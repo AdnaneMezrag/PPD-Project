4184a91e16c1a121d23e6f0cde3c279d0adb3107
Akram Gharbi <akramgharbi13@gmail.com>
2024-04-11 00:56:17 +0100

    FIXED

--- Front-End/yellowApp/App.js
+++ Front-End/yellowApp/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/tabs";
-import Signup from "./screens/Signup";
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Signup />
+      <Tabs />
     </NavigationContainer>
   );
 };
