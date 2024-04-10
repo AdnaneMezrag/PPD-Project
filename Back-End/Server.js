48c50b05954c52e52b2be7cc449d03adf89f5dd5
Akram Gharbi <akramgharbi13@gmail.com>
2024-04-10 01:48:14 +0100

    ADDED SIGNUP IMPLEMENTATION

--- Back-End/Server.js
+++ Back-End/Server.js
@@ -7,12 +7,15 @@ const app = express();
 app.use(express.json()); //almost forgot this
 
 const loginEndPoints = require("./LoginEndpoints");
+const signipEndPoints = require("./SignupEndpoints");
 
 app.use(cors());
 
 const Port = process.env.Port || 4000;
 
-app.use("/api/login", loginEndPoints); // using the other file's endpoints via router
+app.use("/api/login", loginEndPoints); // using the other file's endpoints via router to login and recieve full user data
+
+app.use("/api/signup", signipEndPoints); // using the other file's endpoints via router to singup and add full user data
 
 app.get("/", (req, res) => {
   res.send("Hello");
---
+++ Back-End/SignupEndpoints.js
@@ -0,0 +1,53 @@
+const expres = require("express");
+const route = expres.Router();
+
+const { Client } = require("pg");
+
+const client = new Client({
+  // the values here will be changed accordingly to the final product
+  host: "localhost",
+  password: "123",
+  user: "postgres",
+  database: "ppd",
+  port: "5432",
+});
+
+client.connect();
+
+route.post("/", async (req, res) => {
+  // recieving a request containing user data:{name,email,pswrd,DOB}(id will be generated auto)
+
+  try {
+    const { email, password, username, Dateofbirth, age, gender } = req.body;
+
+    //searching
+    const existingUser = await client.query(
+      "SELECT * FROM patients WHERE email=$1",
+      [email]
+    );
+
+    if (existingUser.rows.length !== 0) {
+      console.log(
+        "Email Adresse '" +
+          email +
+          "' Used ... Please Try Again With a Diffrenet Email..."
+      );
+      return res.json({ userAdded: false }); //for generating a message for the user (front end)
+    }
+
+    const birthday = new Date(Dateofbirth); // this or it will not work...
+
+    const apost = await client.query(
+      "INSERT INTO patients(name,email,password,age,gender,date_of_birth) VALUES($1,$2,$3,$4,$5,$6)",
+      [username, email, password, age, gender, birthday]
+    );
+
+    console.log("User added successfully:", username);
+    res.json({ userAdded: true });
+  } catch (error) {
+    console.error("Error adding user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = route;
