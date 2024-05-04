8b2e7f955f393e670408694f9348e3b37dee4b34
gharbiakram <161060584+gharbiakram@users.noreply.github.com>
2024-05-04 06:19:37 +0100

    Fixing Edit ......

--- SignupEndpoints.js
+++ SignupEndpoints.js
@@ -73,8 +73,9 @@ route.put("/edit", async (req, res) => {
       "UPDATE patients SET name = $1, email = $2, age = $3 WHERE id = $4",
       [name, email, age, id]
     );
+    res.json({nigga : "nigga"});
   } catch (error) {
-    console.error("editing error..." + error);
+    res.status(500).json({ message: "Editing Error ..." });
   }
 });
 //   const query = {
